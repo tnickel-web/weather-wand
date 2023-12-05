51f0cc9da16ca7939fd3b02b41d733430e5f9f38
tnickel-web <timonnickel@web.de>
2023-12-05 21:52:30 +0100

    [BUGFIX] fixed terminal block on unsuccessful requests

--- src/geolocation/geo_api/mod.rs
+++ src/geolocation/geo_api/mod.rs
@@ -2,12 +2,21 @@ pub mod response_handler;
 
 use super::Coordinates;
 use super::Location;
+use std::time::Duration;
 
 pub struct Client {}
 
 impl Client {
     pub async fn fetch(url: String) -> Result<String, Box<dyn std::error::Error>> {
-        let response = reqwest::get(url).await?;
+        let client = reqwest::Client::builder()
+            .timeout(Duration::from_secs(2))
+            .build()?;
+
+        let response = client.get(url).send().await.unwrap_or_else(|_| {
+            println!("Failed to fetch data from the API. You are not connected to the internet or the API is currently not available.");
+            std::process::exit(1);
+        });
+
         let body = response.text().await?;
 
         Ok(body)
--- src/weather/weather_api/mod.rs
+++ src/weather/weather_api/mod.rs
@@ -1,10 +1,20 @@
+use std::time::Duration;
+
 pub mod response_handler;
 
 pub struct Client {}
 
 impl Client {
     pub async fn fetch(url: &str) -> Result<String, Box<dyn std::error::Error>> {
-        let response = reqwest::get(url).await?;
+        let client = reqwest::Client::builder()
+            .timeout(Duration::from_secs(2))
+            .build()?;
+
+        let response = client.get(url).send().await.unwrap_or_else(|_| {
+            println!("Failed to fetch data from the API. You are not connected to the internet or the API is currently not available.");
+            std::process::exit(1);
+        });
+
         let body = response.text().await?;
 
         Ok(body)
